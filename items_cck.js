fd674d1d5d594becc63dff5fcc79f88ddf55ba22
vacalentina <valentina03128@gmail.com>
2025-08-15 12:28:08 -0600

    Update items_cck.js

--- items_cck.js
+++ items_cck.js
@@ -1,308 +1,325 @@
-window.itemsCCK = [
-  {
-    "title": "Checkout Backwall | 2 Ft Header Faros Lm Htu Charolahtu Zyn",
-    "code": "CCK_BACKWALL_2 FT_HEADER_FAROS_LM_HTU_CHAROLAHTU_ZYN",
-    "family": [
-      "2 FT",
-      "HEADER",
-      "FAROS",
-      "LM",
-      "HTU",
-      "CHAROLAHTU",
-      "ZYN"
-    ],
-    "tags": "2 FT HEADER FAROS LM HTU CHAROLAHTU ZYN",
-    "img": "CCK_BACKWALL_2 FT_HEADER_FAROS_LM_HTU_CHAROLAHTU_ZYN.jpg",
-    "categoria": "backwall",
-    "cadena": "cck"
-  },
-  {
-    "title": "Checkout Backwall | 2 Ft Header Faros Lm Htu Zyn",
-    "code": "CCK_BACKWALL_2 FT_HEADER_FAROS_LM_HTU_ZYN",
-    "family": [
-      "2 FT",
-      "HEADER",
-      "FAROS",
-      "LM",
-      "HTU",
-      "ZYN"
-    ],
-    "tags": "2 FT HEADER FAROS LM HTU ZYN",
-    "img": "CCK_BACKWALL_2 FT_HEADER_FAROS_LM_HTU_ZYN.jpg",
-    "categoria": "backwall",
-    "cadena": "cck"
-  },
-  {
-    "title": "Checkout Backwall | 2 Ft Header Faros Lm Reseller Htu Charolahtu Zyn",
-    "code": "CCK_BACKWALL_2 FT_HEADER_FAROS_LM_RESELLER_HTU_CHAROLAHTU_ZYN",
-    "family": [
-      "2 FT",
-      "HEADER",
-      "FAROS",
-      "LM",
-      "RESELLER",
-      "HTU",
-      "CHAROLAHTU",
-      "ZYN"
-    ],
-    "tags": "2 FT HEADER FAROS LM RESELLER HTU CHAROLAHTU ZYN",
-    "img": "CCK_BACKWALL_2 FT_HEADER_FAROS_LM_RESELLER_HTU_CHAROLAHTU_ZYN.jpg",
-    "categoria": "backwall",
-    "cadena": "cck"
-  },
-  {
-    "title": "Checkout Backwall | 2 Ft Header Faros Lm Reseller Htu Zyn",
-    "code": "CCK_BACKWALL_2 FT_HEADER_FAROS_LM_RESELLER_HTU_ZYN",
-    "family": [
-      "2 FT",
-      "HEADER",
-      "FAROS",
-      "LM",
-      "RESELLER",
-      "HTU",
-      "ZYN"
-    ],
-    "tags": "2 FT HEADER FAROS LM RESELLER HTU ZYN",
-    "img": "CCK_BACKWALL_2 FT_HEADER_FAROS_LM_RESELLER_HTU_ZYN.jpg",
-    "categoria": "backwall",
-    "cadena": "cck"
-  },
-  {
-    "title": "Checkout Backwall | 3 Ft Header Faros Lm Htu Charolahtu Zyn",
-    "code": "CCK_BACKWALL_3 FT_HEADER_FAROS_LM_HTU_CHAROLAHTU_ZYN",
-    "family": [
-      "3 FT",
-      "HEADER",
-      "FAROS",
-      "LM",
-      "HTU",
-      "CHAROLAHTU",
-      "ZYN"
-    ],
-    "tags": "3 FT HEADER FAROS LM HTU CHAROLAHTU ZYN",
-    "img": "CCK_BACKWALL_3 FT_HEADER_FAROS_LM_HTU_CHAROLAHTU_ZYN.jpg",
-    "categoria": "backwall",
-    "cadena": "cck"
-  },
-  {
-    "title": "Checkout Backwall | 3 Ft Header Faros Lm Htu Zyn",
-    "code": "CCK_BACKWALL_3 FT_HEADER_FAROS_LM_HTU_ZYN",
-    "family": [
-      "3 FT",
-      "HEADER",
-      "FAROS",
-      "LM",
-      "HTU",
-      "ZYN"
-    ],
-    "tags": "3 FT HEADER FAROS LM HTU ZYN",
-    "img": "CCK_BACKWALL_3 FT_HEADER_FAROS_LM_HTU_ZYN.jpg",
-    "categoria": "backwall",
-    "cadena": "cck"
-  },
-  {
-    "title": "Checkout Backwall | 3 Ft Header Faros Lm Reseller Htu Charolahtu Zyn",
-    "code": "CCK_BACKWALL_3 FT_HEADER_FAROS_LM_RESELLER_HTU_CHAROLAHTU_ZYN",
-    "family": [
-      "3 FT",
-      "HEADER",
-      "FAROS",
-      "LM",
-      "RESELLER",
-      "HTU",
-      "CHAROLAHTU",
-      "ZYN"
-    ],
-    "tags": "3 FT HEADER FAROS LM RESELLER HTU CHAROLAHTU ZYN",
-    "img": "CCK_BACKWALL_3 FT_HEADER_FAROS_LM_RESELLER_HTU_CHAROLAHTU_ZYN.jpg",
-    "categoria": "backwall",
-    "cadena": "cck"
-  },
-  {
-    "title": "Checkout Backwall | 4 Ft Header Faros Lm Htu Charolahtu Zyn",
-    "code": "CCK_BACKWALL_4 FT_HEADER_FAROS_LM_HTU_CHAROLAHTU_ZYN",
-    "family": [
-      "4 FT",
-      "HEADER",
-      "FAROS",
-      "LM",
-      "HTU",
-      "CHAROLAHTU",
-      "ZYN"
-    ],
-    "tags": "4 FT HEADER FAROS LM HTU CHAROLAHTU ZYN",
-    "img": "CCK_BACKWALL_4 FT_HEADER_FAROS_LM_HTU_CHAROLAHTU_ZYN.jpg",
-    "categoria": "backwall",
-    "cadena": "cck"
-  },
-  {
-    "title": "Checkout Backwall | 4 Ft Header Faros Lm Htu Zyn",
-    "code": "CCK_BACKWALL_4 FT_HEADER_FAROS_LM_HTU_ZYN",
-    "family": [
-      "4 FT",
-      "HEADER",
-      "FAROS",
-      "LM",
-      "HTU",
-      "ZYN"
-    ],
-    "tags": "4 FT HEADER FAROS LM HTU ZYN",
-    "img": "CCK_BACKWALL_4 FT_HEADER_FAROS_LM_HTU_ZYN.jpg",
-    "categoria": "backwall",
-    "cadena": "cck"
-  },
-  {
-    "title": "Checkout Backwall | 4 Ft Header Faros Lm Reseller Htu Charolahtu Zyn",
-    "code": "CCK_BACKWALL_4 FT_HEADER_FAROS_LM_RESELLER_HTU_CHAROLAHTU_ZYN",
-    "family": [
-      "4 FT",
-      "HEADER",
-      "FAROS",
-      "LM",
-      "RESELLER",
-      "HTU",
-      "CHAROLAHTU",
-      "ZYN"
-    ],
-    "tags": "4 FT HEADER FAROS LM RESELLER HTU CHAROLAHTU ZYN",
-    "img": "CCK_BACKWALL_4 FT_HEADER_FAROS_LM_RESELLER_HTU_CHAROLAHTU_ZYN.jpg",
-    "categoria": "backwall",
-    "cadena": "cck"
-  },
-  {
-    "title": "Checkout Backwall | 5 Ft Header Faros Lm Htu Charolahtu Zyn",
-    "code": "CCK_BACKWALL_5 FT_HEADER_FAROS_LM_HTU_CHAROLAHTU_ZYN",
-    "family": [
-      "5 FT",
-      "HEADER",
-      "FAROS",
-      "LM",
-      "HTU",
-      "CHAROLAHTU",
-      "ZYN"
-    ],
-    "tags": "5 FT HEADER FAROS LM HTU CHAROLAHTU ZYN",
-    "img": "CCK_BACKWALL_5 FT_HEADER_FAROS_LM_HTU_CHAROLAHTU_ZYN.jpg",
-    "categoria": "backwall",
-    "cadena": "cck"
-  },
-  {
-    "title": "Checkout Backwall | 5 Ft Header Faros Lm Htu Zyn",
-    "code": "CCK_BACKWALL_5 FT_HEADER_FAROS_LM_HTU_ZYN",
-    "family": [
-      "5 FT",
-      "HEADER",
-      "FAROS",
-      "LM",
-      "HTU",
-      "ZYN"
-    ],
-    "tags": "5 FT HEADER FAROS LM HTU ZYN",
-    "img": "CCK_BACKWALL_5 FT_HEADER_FAROS_LM_HTU_ZYN.jpg",
-    "categoria": "backwall",
-    "cadena": "cck"
-  },
-  {
-    "title": "Checkout Backwall | 5 Ft Header Faros Lm Reseller Htu Charolahtu Zyn",
-    "code": "CCK_BACKWALL_5 FT_HEADER_FAROS_LM_RESELLER_HTU_CHAROLAHTU_ZYN",
-    "family": [
-      "5 FT",
-      "HEADER",
-      "FAROS",
-      "LM",
-      "RESELLER",
-      "HTU",
-      "CHAROLAHTU",
-      "ZYN"
-    ],
-    "tags": "5 FT HEADER FAROS LM RESELLER HTU CHAROLAHTU ZYN",
-    "img": "CCK_BACKWALL_5 FT_HEADER_FAROS_LM_RESELLER_HTU_CHAROLAHTU_ZYN.jpg",
-    "categoria": "backwall",
-    "cadena": "cck"
-  },
-  {
-    "title": "Checkout Backwall | 5 Ft Header Faros Lm Reseller Htu Zyn",
-    "code": "CCK_BACKWALL_5 FT_HEADER_FAROS_LM_RESELLER_HTU_ZYN",
-    "family": [
-      "5 FT",
-      "HEADER",
-      "FAROS",
-      "LM",
-      "RESELLER",
-      "HTU",
-      "ZYN"
-    ],
-    "tags": "5 FT HEADER FAROS LM RESELLER HTU ZYN",
-    "img": "CCK_BACKWALL_5 FT_HEADER_FAROS_LM_RESELLER_HTU_ZYN.jpg",
-    "categoria": "backwall",
-    "cadena": "cck"
-  },
-  {
-    "title": "Checkout Backwall | 6 Ft Header Faros Lm Htu Charolahtu Zyn",
-    "code": "CCK_BACKWALL_6 FT_HEADER_FAROS_LM_HTU_CHAROLAHTU_ZYN",
-    "family": [
-      "6 FT",
-      "HEADER",
-      "FAROS",
-      "LM",
-      "HTU",
-      "CHAROLAHTU",
-      "ZYN"
-    ],
-    "tags": "6 FT HEADER FAROS LM HTU CHAROLAHTU ZYN",
-    "img": "CCK_BACKWALL_6 FT_HEADER_FAROS_LM_HTU_CHAROLAHTU_ZYN.jpg",
-    "categoria": "backwall",
-    "cadena": "cck"
-  },
-  {
-    "title": "Checkout Backwall | 6 Ft Header Faros Lm Htu Zyn",
-    "code": "CCK_BACKWALL_6 FT_HEADER_FAROS_LM_HTU_ZYN",
-    "family": [
-      "6 FT",
-      "HEADER",
-      "FAROS",
-      "LM",
-      "HTU",
-      "ZYN"
-    ],
-    "tags": "6 FT HEADER FAROS LM HTU ZYN",
-    "img": "CCK_BACKWALL_6 FT_HEADER_FAROS_LM_HTU_ZYN.jpg",
-    "categoria": "backwall",
-    "cadena": "cck"
-  },
-  {
-    "title": "Checkout Backwall | 6 Ft Header Faros Lm Reseller Htu Charolahtu Zyn",
-    "code": "CCK_BACKWALL_6 FT_HEADER_FAROS_LM_RESELLER_HTU_CHAROLAHTU_ZYN",
-    "family": [
-      "6 FT",
-      "HEADER",
-      "FAROS",
-      "LM",
-      "RESELLER",
-      "HTU",
-      "CHAROLAHTU",
-      "ZYN"
-    ],
-    "tags": "6 FT HEADER FAROS LM RESELLER HTU CHAROLAHTU ZYN",
-    "img": "CCK_BACKWALL_6 FT_HEADER_FAROS_LM_RESELLER_HTU_CHAROLAHTU_ZYN.jpg",
-    "categoria": "backwall",
-    "cadena": "cck"
-  },
-  {
-    "title": "Checkout Backwall | 7 Ft Header Faros Lm Htu Charolahtu Zyn",
-    "code": "CCK_BACKWALL_7 FT_HEADER_FAROS_LM_HTU_CHAROLAHTU_ZYN",
-    "family": [
-      "7 FT",
-      "HEADER",
-      "FAROS",
-      "LM",
-      "HTU",
-      "CHAROLAHTU",
-      "ZYN"
-    ],
-    "tags": "7 FT HEADER FAROS LM HTU CHAROLAHTU ZYN",
-    "img": "CCK_BACKWALL_7 FT_HEADER_FAROS_LM_HTU_CHAROLAHTU_ZYN.jpg",
-    "categoria": "backwall",
-    "cadena": "cck"
-  }
+window.itemsCCK = [{
+        "title": "Checkout Backwall | Backwall 2 Ft Header Faros Lm Htu Charolahtu Zyn",
+        "code": "CCK_BACKWALL_2 FT_HEADER_FAROS_LM_HTU_CHAROLAHTU_ZYN",
+        "family": [
+            "BACKWALL",
+            "2 FT",
+            "HEADER",
+            "FAROS",
+            "LM",
+            "HTU",
+            "CHAROLAHTU",
+            "ZYN"
+        ],
+        "tags": "BACKWALL 2 FT HEADER FAROS LM HTU CHAROLAHTU ZYN",
+        "img": "CCK_BACKWALL_2 FT_HEADER_FAROS_LM_HTU_CHAROLAHTU_ZYN.jpg",
+        "categoria": "backwall",
+        "cadena": "cck"
+    },
+    {
+        "title": "Checkout Backwall | Backwall 2 Ft Header Faros Lm Htu Zyn",
+        "code": "CCK_BACKWALL_2 FT_HEADER_FAROS_LM_HTU_ZYN",
+        "family": [
+            "BACKWALL",
+            "2 FT",
+            "HEADER",
+            "FAROS",
+            "LM",
+            "HTU",
+            "ZYN"
+        ],
+        "tags": "BACKWALL 2 FT HEADER FAROS LM HTU ZYN",
+        "img": "CCK_BACKWALL_2 FT_HEADER_FAROS_LM_HTU_ZYN.jpg",
+        "categoria": "backwall",
+        "cadena": "cck"
+    },
+    {
+        "title": "Checkout Backwall | Backwall 2 Ft Header Faros Lm Reseller Htu Charolahtu Zyn",
+        "code": "CCK_BACKWALL_2 FT_HEADER_FAROS_LM_RESELLER_HTU_CHAROLAHTU_ZYN",
+        "family": [
+            "BACKWALL",
+            "2 FT",
+            "HEADER",
+            "FAROS",
+            "LM",
+            "RESELLER",
+            "HTU",
+            "CHAROLAHTU",
+            "ZYN"
+        ],
+        "tags": "BACKWALL 2 FT HEADER FAROS LM RESELLER HTU CHAROLAHTU ZYN",
+        "img": "CCK_BACKWALL_2 FT_HEADER_FAROS_LM_RESELLER_HTU_CHAROLAHTU_ZYN.jpg",
+        "categoria": "backwall",
+        "cadena": "cck"
+    },
+    {
+        "title": "Checkout Backwall | Backwall 2 Ft Header Faros Lm Reseller Htu Zyn",
+        "code": "CCK_BACKWALL_2 FT_HEADER_FAROS_LM_RESELLER_HTU_ZYN",
+        "family": [
+            "BACKWALL",
+            "2 FT",
+            "HEADER",
+            "FAROS",
+            "LM",
+            "RESELLER",
+            "HTU",
+            "ZYN"
+        ],
+        "tags": "BACKWALL 2 FT HEADER FAROS LM RESELLER HTU ZYN",
+        "img": "CCK_BACKWALL_2 FT_HEADER_FAROS_LM_RESELLER_HTU_ZYN.jpg",
+        "categoria": "backwall",
+        "cadena": "cck"
+    },
+    {
+        "title": "Checkout Backwall | Backwall 3 Ft Header Faros Lm Htu Charolahtu Zyn",
+        "code": "CCK_BACKWALL_3 FT_HEADER_FAROS_LM_HTU_CHAROLAHTU_ZYN",
+        "family": [
+            "BACKWALL",
+            "3 FT",
+            "HEADER",
+            "FAROS",
+            "LM",
+            "HTU",
+            "CHAROLAHTU",
+            "ZYN"
+        ],
+        "tags": "BACKWALL 3 FT HEADER FAROS LM HTU CHAROLAHTU ZYN",
+        "img": "CCK_BACKWALL_3 FT_HEADER_FAROS_LM_HTU_CHAROLAHTU_ZYN.jpg",
+        "categoria": "backwall",
+        "cadena": "cck"
+    },
+    {
+        "title": "Checkout Backwall | Backwall 3 Ft Header Faros Lm Htu Zyn",
+        "code": "CCK_BACKWALL_3 FT_HEADER_FAROS_LM_HTU_ZYN",
+        "family": [
+            "BACKWALL",
+            "3 FT",
+            "HEADER",
+            "FAROS",
+            "LM",
+            "HTU",
+            "ZYN"
+        ],
+        "tags": "BACKWALL 3 FT HEADER FAROS LM HTU ZYN",
+        "img": "CCK_BACKWALL_3 FT_HEADER_FAROS_LM_HTU_ZYN.jpg",
+        "categoria": "backwall",
+        "cadena": "cck"
+    },
+    {
+        "title": "Checkout Backwall | Backwall 3 Ft Header Faros Lm Reseller Htu Charolahtu Zyn",
+        "code": "CCK_BACKWALL_3 FT_HEADER_FAROS_LM_RESELLER_HTU_CHAROLAHTU_ZYN",
+        "family": [
+            "BACKWALL",
+            "3 FT",
+            "HEADER",
+            "FAROS",
+            "LM",
+            "RESELLER",
+            "HTU",
+            "CHAROLAHTU",
+            "ZYN"
+        ],
+        "tags": "BACKWALL 3 FT HEADER FAROS LM RESELLER HTU CHAROLAHTU ZYN",
+        "img": "CCK_BACKWALL_3 FT_HEADER_FAROS_LM_RESELLER_HTU_CHAROLAHTU_ZYN.jpg",
+        "categoria": "backwall",
+        "cadena": "cck"
+    },
+    {
+        "title": "Checkout Backwall | Backwall 4 Ft Header Faros Lm Htu Charolahtu Zyn",
+        "code": "CCK_BACKWALL_4 FT_HEADER_FAROS_LM_HTU_CHAROLAHTU_ZYN",
+        "family": [
+            "BACKWALL",
+            "4 FT",
+            "HEADER",
+            "FAROS",
+            "LM",
+            "HTU",
+            "CHAROLAHTU",
+            "ZYN"
+        ],
+        "tags": "BACKWALL 4 FT HEADER FAROS LM HTU CHAROLAHTU ZYN",
+        "img": "CCK_BACKWALL_4 FT_HEADER_FAROS_LM_HTU_CHAROLAHTU_ZYN.jpg",
+        "categoria": "backwall",
+        "cadena": "cck"
+    },
+    {
+        "title": "Checkout Backwall | Backwall 4 Ft Header Faros Lm Htu Zyn",
+        "code": "CCK_BACKWALL_4 FT_HEADER_FAROS_LM_HTU_ZYN",
+        "family": [
+            "BACKWALL",
+            "4 FT",
+            "HEADER",
+            "FAROS",
+            "LM",
+            "HTU",
+            "ZYN"
+        ],
+        "tags": "BACKWALL 4 FT HEADER FAROS LM HTU ZYN",
+        "img": "CCK_BACKWALL_4 FT_HEADER_FAROS_LM_HTU_ZYN.jpg",
+        "categoria": "backwall",
+        "cadena": "cck"
+    },
+    {
+        "title": "Checkout Backwall | Backwall 4 Ft Header Faros Lm Reseller Htu Charolahtu Zyn",
+        "code": "CCK_BACKWALL_4 FT_HEADER_FAROS_LM_RESELLER_HTU_CHAROLAHTU_ZYN",
+        "family": [
+            "BACKWALL",
+            "4 FT",
+            "HEADER",
+            "FAROS",
+            "LM",
+            "RESELLER",
+            "HTU",
+            "CHAROLAHTU",
+            "ZYN"
+        ],
+        "tags": "BACKWALL 4 FT HEADER FAROS LM RESELLER HTU CHAROLAHTU ZYN",
+        "img": "CCK_BACKWALL_4 FT_HEADER_FAROS_LM_RESELLER_HTU_CHAROLAHTU_ZYN.jpg",
+        "categoria": "backwall",
+        "cadena": "cck"
+    },
+    {
+        "title": "Checkout Backwall | Backwall 5 Ft Header Faros Lm Htu Charolahtu Zyn",
+        "code": "CCK_BACKWALL_5 FT_HEADER_FAROS_LM_HTU_CHAROLAHTU_ZYN",
+        "family": [
+            "BACKWALL",
+            "5 FT",
+            "HEADER",
+            "FAROS",
+            "LM",
+            "HTU",
+            "CHAROLAHTU",
+            "ZYN"
+        ],
+        "tags": "BACKWALL 5 FT HEADER FAROS LM HTU CHAROLAHTU ZYN",
+        "img": "CCK_BACKWALL_5 FT_HEADER_FAROS_LM_HTU_CHAROLAHTU_ZYN.jpg",
+        "categoria": "backwall",
+        "cadena": "cck"
+    },
+    {
+        "title": "Checkout Backwall | Backwall 5 Ft Header Faros Lm Htu Zyn",
+        "code": "CCK_BACKWALL_5 FT_HEADER_FAROS_LM_HTU_ZYN",
+        "family": [
+            "BACKWALL",
+            "5 FT",
+            "HEADER",
+            "FAROS",
+            "LM",
+            "HTU",
+            "ZYN"
+        ],
+        "tags": "BACKWALL 5 FT HEADER FAROS LM HTU ZYN",
+        "img": "CCK_BACKWALL_5 FT_HEADER_FAROS_LM_HTU_ZYN.jpg",
+        "categoria": "backwall",
+        "cadena": "cck"
+    },
+    {
+        "title": "Checkout Backwall | Backwall 5 Ft Header Faros Lm Reseller Htu Charolahtu Zyn",
+        "code": "CCK_BACKWALL_5 FT_HEADER_FAROS_LM_RESELLER_HTU_CHAROLAHTU_ZYN",
+        "family": [
+            "BACKWALL",
+            "5 FT",
+            "HEADER",
+            "FAROS",
+            "LM",
+            "RESELLER",
+            "HTU",
+            "CHAROLAHTU",
+            "ZYN"
+        ],
+        "tags": "BACKWALL 5 FT HEADER FAROS LM RESELLER HTU CHAROLAHTU ZYN",
+        "img": "CCK_BACKWALL_5 FT_HEADER_FAROS_LM_RESELLER_HTU_CHAROLAHTU_ZYN.jpg",
+        "categoria": "backwall",
+        "cadena": "cck"
+    },
+    {
+        "title": "Checkout Backwall | Backwall 5 Ft Header Faros Lm Reseller Htu Zyn",
+        "code": "CCK_BACKWALL_5 FT_HEADER_FAROS_LM_RESELLER_HTU_ZYN",
+        "family": [
+            "BACKWALL",
+            "5 FT",
+            "HEADER",
+            "FAROS",
+            "LM",
+            "RESELLER",
+            "HTU",
+            "ZYN"
+        ],
+        "tags": "BACKWALL 5 FT HEADER FAROS LM RESELLER HTU ZYN",
+        "img": "CCK_BACKWALL_5 FT_HEADER_FAROS_LM_RESELLER_HTU_ZYN.jpg",
+        "categoria": "backwall",
+        "cadena": "cck"
+    },
+    {
+        "title": "Checkout Backwall | Backwall 6 Ft Header Faros Lm Htu Charolahtu Zyn",
+        "code": "CCK_BACKWALL_6 FT_HEADER_FAROS_LM_HTU_CHAROLAHTU_ZYN",
+        "family": [
+            "BACKWALL",
+            "6 FT",
+            "HEADER",
+            "FAROS",
+            "LM",
+            "HTU",
+            "CHAROLAHTU",
+            "ZYN"
+        ],
+        "tags": "BACKWALL 6 FT HEADER FAROS LM HTU CHAROLAHTU ZYN",
+        "img": "CCK_BACKWALL_6 FT_HEADER_FAROS_LM_HTU_CHAROLAHTU_ZYN.jpg",
+        "categoria": "backwall",
+        "cadena": "cck"
+    },
+    {
+        "title": "Checkout Backwall | Backwall 6 Ft Header Faros Lm Htu Zyn",
+        "code": "CCK_BACKWALL_6 FT_HEADER_FAROS_LM_HTU_ZYN",
+        "family": [
+            "BACKWALL",
+            "6 FT",
+            "HEADER",
+            "FAROS",
+            "LM",
+            "HTU",
+            "ZYN"
+        ],
+        "tags": "BACKWALL 6 FT HEADER FAROS LM HTU ZYN",
+        "img": "CCK_BACKWALL_6 FT_HEADER_FAROS_LM_HTU_ZYN.jpg",
+        "categoria": "backwall",
+        "cadena": "cck"
+    },
+    {
+        "title": "Checkout Backwall | Backwall 6 Ft Header Faros Lm Reseller Htu Charolahtu Zyn",
+        "code": "CCK_BACKWALL_6 FT_HEADER_FAROS_LM_RESELLER_HTU_CHAROLAHTU_ZYN",
+        "family": [
+            "BACKWALL",
+            "6 FT",
+            "HEADER",
+            "FAROS",
+            "LM",
+            "RESELLER",
+            "HTU",
+            "CHAROLAHTU",
+            "ZYN"
+        ],
+        "tags": "BACKWALL 6 FT HEADER FAROS LM RESELLER HTU CHAROLAHTU ZYN",
+        "img": "CCK_BACKWALL_6 FT_HEADER_FAROS_LM_RESELLER_HTU_CHAROLAHTU_ZYN.jpg",
+        "categoria": "backwall",
+        "cadena": "cck"
+    },
+    {
+        "title": "Checkout Backwall | Backwall 7 Ft Header Faros Lm Htu Charolahtu Zyn",
+        "code": "CCK_BACKWALL_7 FT_HEADER_FAROS_LM_HTU_CHAROLAHTU_ZYN",
+        "family": [
+            "BACKWALL",
+            "7 FT",
+            "HEADER",
+            "FAROS",
+            "LM",
+            "HTU",
+            "CHAROLAHTU",
+            "ZYN"
+        ],
+        "tags": "BACKWALL 7 FT HEADER FAROS LM HTU CHAROLAHTU ZYN",
+        "img": "CCK_BACKWALL_7 FT_HEADER_FAROS_LM_HTU_CHAROLAHTU_ZYN.jpg",
+        "categoria": "backwall",
+        "cadena": "cck"
+    }
 ];
