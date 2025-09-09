f084b19f5ed88cae0e0f20e7da1125f9aa77758b
vacalentina <valentina03128@gmail.com>
2025-09-09 10:41:45 -0600

    Update items_seven.js

--- items_seven.js
+++ items_seven.js
@@ -1023,5 +1023,86 @@ window.itemsSeven = [{
         "img": "711_BACKWALL_6 FT_HEADER_LM_RESELLER_HTU_CHAROLAHTU_ZYN.jpg",
         "categoria": "backwall",
         "cadena": "711"
+    },
+        {
+        "title": "NS1 | RESELLER | LM | FAROS | 25",
+        "code": "0925_711_NS1_RESELLER_LM_FAROS_25",
+        "family": [
+            "NS1",
+            "RESELLER",
+            "LM",
+            "FAROS",
+            "25"
+        ],
+        "tags": "NS1 RESELLER LM FAROS 25",
+        "img": "0925_711_NS1_RESELLER_LM_FAROS_25.jpg",
+        "categoria": "ns1",
+        "cadena": "711"
+    },
+    {
+        "title": "NS1 | RESELLER | LM | FAROS",
+        "code": "0925_711_NS1_RESELLER_LM_FAROS",
+        "family": [
+            "NS1",
+            "RESELLER",
+            "LM",
+            "FAROS"
+        ],
+        "tags": "NS1 RESELLER LM FAROS",
+        "img": "0925_711_NS1_RESELLER_LM_FAROS.jpg",
+        "categoria": "ns1",
+        "cadena": "711"
+    }, {
+        "title": "NS1 | LM | FAROS",
+        "code": "0925_711_NS1_LM_FAROS",
+        "family": [
+            "NS1",
+            "LM",
+            "FAROS"
+        ],
+        "tags": "NS1 LM FAROS",
+        "img": "0925_711_NS1_LM_FAROS.jpg",
+        "categoria": "ns1",
+        "cadena": "711"
+    },
+    {
+        "title": "NS1 | LM | FAROS | 25",
+        "code": "0925_711_NS1_LM_FAROS_25",
+        "family": [
+            "NS1",
+            "LM",
+            "FAROS",
+            "25"
+        ],
+        "tags": "NS1 LM FAROS 25",
+        "img": "0925_711_NS1_LM_FAROS_25.jpg",
+        "categoria": "ns1",
+        "cadena": "711"
+    },
+    {
+        "title": "NS1 | RESELLER | LM",
+        "code": "0925_711_NS1_RESELLER_LM",
+        "family": [
+            "NS1",
+            "RESELLER",
+            "LM"
+        ],
+        "tags": "NS1 RESELLER LM",
+        "img": "0925_711_NS1_RESELLER_LM.jpg",
+        "categoria": "ns1",
+        "cadena": "711"
+    },
+    {
+        "title": "NS1 | LM",
+        "code": "0925_711_NS1_LM",
+        "family": [
+            "NS1",
+            "LM"
+        ],
+        "tags": "NS1 LM",
+        "img": "0925_711_NS1_LM.jpg",
+        "categoria": "ns1",
+        "cadena": "711"
     }
 ];
+
