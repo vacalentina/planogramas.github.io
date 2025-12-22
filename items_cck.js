a9ec53c0151fded9d16866703bf94318d0c240a1
vacalentina <valentina03128@gmail.com>
2025-12-22 11:06:26 -0600

    Update items_cck.js

--- items_cck.js
+++ items_cck.js
@@ -355,5 +355,134 @@ window.itemsCCK = [{
         "img": "1125_CCK_BWLL_7 FT_HEADER_FAROS_LM--RESELLER_HTU_ZYN.jpg",
         "categoria": "backwall",
         "cadena": "cck"
-    }
+
+    },
+    {
+  title: "Checkout Backwall | 2 | FT | HEADER | FAROS | 25 | LM | HTU | ZYN",
+  code: "CCK_BACKWALL_2_FT_HEADER_FAROS_25_LM_HTU_ZYN",
+  family: ["2", "FT", "HEADER", "FAROS", "25", "LM", "HTU", "ZYN"],
+  tags: "2 FT HEADER FAROS 25 LM HTU ZYN",
+  img: "ENE26_CCK_2_FT_HEADER_FAROS25_LM--HTU_ZYN.png",
+  categoria: "backwall",
+  cadena: "cck",
+  month: "ENE26"
+},
+{
+  title: "Checkout Backwall | 2 | FT | HEADER | FAROS | 25 | LM | RESELLER | HTU | ZYN",
+  code: "CCK_BACKWALL_2_FT_HEADER_FAROS_25_LM_RESELLER_HTU_ZYN",
+  family: ["2", "FT", "HEADER", "FAROS", "25", "LM", "RESELLER", "HTU", "ZYN"],
+  tags: "2 FT HEADER FAROS 25 LM RESELLER HTU ZYN",
+  img: "ENE26_CCK_2_FT_HEADER_FAROS25_LM--RESELLER_HTU_ZYN.png",
+  categoria: "backwall",
+  cadena: "cck",
+  month: "ENE26"
+},
+
+{
+  title: "Checkout Backwall | 3 | FT | HEADER | FAROS | 25 | LM | HTU | ZYN",
+  code: "CCK_BACKWALL_3_FT_HEADER_FAROS_25_LM_HTU_ZYN",
+  family: ["3", "FT", "HEADER", "FAROS", "25", "LM", "HTU", "ZYN"],
+  tags: "3 FT HEADER FAROS 25 LM HTU ZYN",
+  img: "ENE26_CCK_3_FT_HEADER_FAROS25_LM--HTU_ZYN.png",
+  categoria: "backwall",
+  cadena: "cck",
+  month: "ENE26"
+},
+{
+  title: "Checkout Backwall | 3 | FT | HEADER | FAROS | 25 | LM | RESELLER | HTU | ZYN",
+  code: "CCK_BACKWALL_3_FT_HEADER_FAROS_25_LM_RESELLER_HTU_ZYN",
+  family: ["3", "FT", "HEADER", "FAROS", "25", "LM", "RESELLER", "HTU", "ZYN"],
+  tags: "3 FT HEADER FAROS 25 LM RESELLER HTU ZYN",
+  img: "ENE26_CCK_3_FT_HEADER_FAROS25_LM--RESELLER_HTU_ZYN.png",
+  categoria: "backwall",
+  cadena: "cck",
+  month: "ENE26"
+},
+
+{
+  title: "Checkout Backwall | 4 | FT | HEADER | FAROS | 25 | LM | HTU | ZYN",
+  code: "CCK_BACKWALL_4_FT_HEADER_FAROS_25_LM_HTU_ZYN",
+  family: ["4", "FT", "HEADER", "FAROS", "25", "LM", "HTU", "ZYN"],
+  tags: "4 FT HEADER FAROS 25 LM HTU ZYN",
+  img: "ENE26_CCK_4_FT_HEADER_FAROS25_LM--HTU_ZYN.png",
+  categoria: "backwall",
+  cadena: "cck",
+  month: "ENE26"
+},
+{
+  title: "Checkout Backwall | 4 | FT | HEADER | FAROS | 25 | LM | RESELLER | HTU | ZYN",
+  code: "CCK_BACKWALL_4_FT_HEADER_FAROS_25_LM_RESELLER_HTU_ZYN",
+  family: ["4", "FT", "HEADER", "FAROS", "25", "LM", "RESELLER", "HTU", "ZYN"],
+  tags: "4 FT HEADER FAROS 25 LM RESELLER HTU ZYN",
+  img: "ENE26_CCK_4_FT_HEADER_FAROS25_LM--RESELLER_HTU_ZYN.png",
+  categoria: "backwall",
+  cadena: "cck",
+  month: "ENE26"
+},
+
+{
+  title: "Checkout Backwall | 5 | FT | HEADER | FAROS | 25 | LM | HTU | ZYN",
+  code: "CCK_BACKWALL_5_FT_HEADER_FAROS_25_LM_HTU_ZYN",
+  family: ["5", "FT", "HEADER", "FAROS", "25", "LM", "HTU", "ZYN"],
+  tags: "5 FT HEADER FAROS 25 LM HTU ZYN",
+  img: "ENE26_CCK_5_FT_HEADER_FAROS25_LM--HTU_ZYN.png",
+  categoria: "backwall",
+  cadena: "cck",
+  month: "ENE26"
+},
+{
+  title: "Checkout Backwall | 5 | FT | HEADER | FAROS | 25 | LM | RESELLER | HTU | ZYN",
+  code: "CCK_BACKWALL_5_FT_HEADER_FAROS_25_LM_RESELLER_HTU_ZYN",
+  family: ["5", "FT", "HEADER", "FAROS", "25", "LM", "RESELLER", "HTU", "ZYN"],
+  tags: "5 FT HEADER FAROS 25 LM RESELLER HTU ZYN",
+  img: "ENE26_CCK_5_FT_HEADER_FAROS25_LM--RESELLER_HTU_ZYN.png",
+  categoria: "backwall",
+  cadena: "cck",
+  month: "ENE26"
+},
+
+{
+  title: "Checkout Backwall | 6 | FT | HEADER | FAROS | 25 | LM | HTU | ZYN",
+  code: "CCK_BACKWALL_6_FT_HEADER_FAROS_25_LM_HTU_ZYN",
+  family: ["6", "FT", "HEADER", "FAROS", "25", "LM", "HTU", "ZYN"],
+  tags: "6 FT HEADER FAROS 25 LM HTU ZYN",
+  img: "ENE26_CCK_6_FT_HEADER_FAROS25_LM--HTU_ZYN.png",
+  categoria: "backwall",
+  cadena: "cck",
+  month: "ENE26"
+},
+{
+  title: "Checkout Backwall | 6 | FT | HEADER | FAROS | 25 | LM | RESELLER | HTU | ZYN",
+  code: "CCK_BACKWALL_6_FT_HEADER_FAROS_25_LM_RESELLER_HTU_ZYN",
+  family: ["6", "FT", "HEADER", "FAROS", "25", "LM", "RESELLER", "HTU", "ZYN"],
+  tags: "6 FT HEADER FAROS 25 LM RESELLER HTU ZYN",
+  img: "ENE26_CCK_6_FT_HEADER_FAROS25_LM--RESELLER_HTU_ZYN.png",
+  categoria: "backwall",
+  cadena: "cck",
+  month: "ENE26"
+},
+
+{
+  title: "Checkout Backwall | 7 | FT | HEADER | FAROS | 25 | LM | HTU | ZYN",
+  code: "CCK_BACKWALL_7_FT_HEADER_FAROS_25_LM_HTU_ZYN",
+  family: ["7", "FT", "HEADER", "FAROS", "25", "LM", "HTU", "ZYN"],
+  tags: "7 FT HEADER FAROS 25 LM HTU ZYN",
+  img: "ENE26_CCK_7_FT_HEADER_FAROS25_LM--HTU_ZYN.png",
+  categoria: "backwall",
+  cadena: "cck",
+  month: "ENE26"
+},
+{
+  title: "Checkout Backwall | 7 | FT | HEADER | FAROS | 25 | LM | RESELLER | HTU | ZYN",
+  code: "CCK_BACKWALL_7_FT_HEADER_FAROS_25_LM_RESELLER_HTU_ZYN",
+  family: ["7", "FT", "HEADER", "FAROS", "25", "LM", "RESELLER", "HTU", "ZYN"],
+  tags: "7 FT HEADER FAROS 25 LM RESELLER HTU ZYN",
+  img: "ENE26_CCK_7_FT_HEADER_FAROS25_LM--RESELLER_HTU_ZYN.png",
+  categoria: "backwall",
+  cadena: "cck",
+  month: "ENE26"
+}
+
+
+
 ]
