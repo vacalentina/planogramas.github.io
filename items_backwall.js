c1c7d8f5a5a1571e21d42451d381d0fa4ffb5ce7
vacalentina <valentina03128@gmail.com>
2025-12-22 11:05:44 -0600

    Update items_backwall.js

--- items_backwall.js
+++ items_backwall.js
@@ -5,7 +5,8 @@ window.itemsBackwall = [{
             "OXXO",
             "BWLL",
             "4 CHAROLAS",
-            "HEADER"
+            "HEADER",
+           
         ],
         tags: "OXXO BWLL 4 CHAROLAS HEADER",
         img: "DIC25_OXXO_4_CHAR_HEADER-.png",
@@ -387,7 +388,7 @@ window.itemsBackwall = [{
         img: "DIC25_OXXO_5_CHAR_HEADER-ZYN_CHARZYN25.png",
         categoria: "backwall",
         cadena: "oxxo"
-    }
+    },
     /* {
          title: "BACKWALL | BWLL | HEADER | COMPETENCIA",
          code: "OXXO_OXXO_BACKWALL_HEADER_COMPETENCIA",
@@ -402,4 +403,250 @@ window.itemsBackwall = [{
          categoria: "backwall",
          cadena: "oxxo"
      }*/
+  // ...items existentes,
+{
+  title: "BACKWALL | BWLL | 4 CHAROLAS | HEADER | BASE |- ENE",
+  code: "OXXO_OXXO_BACKWALL_4CHAROLAS_HEADER_BASE_ENE",
+  family: ["OXXO", "BWLL", "4 CHAROLAS", "HEADER", "BASE", "ENE"],
+  tags: "OXXO BWLL 4 CHAROLAS HEADER BASE ENE",
+  img: "ENE26_OXXO_4_CHAR_HEADER-.png",
+  categoria: "backwall",
+  cadena: "oxxo",
+  month: "ENE26"
+},
+{
+  title: "BACKWALL | BWLL | 4 CHAROLAS | HEADER | HTU |- ENE",
+  code: "OXXO_OXXO_BACKWALL_4CHAROLAS_HEADER_HTU_ENE",
+  family: ["OXXO", "BWLL", "4 CHAROLAS", "HEADER", "HTU", "ENE"],
+  tags: "OXXO BWLL 4 CHAROLAS HEADER HTU ENE",
+  img: "ENE26_OXXO_4_CHAR_HEADER-HTU.png",
+  categoria: "backwall",
+  cadena: "oxxo",
+  month: "ENE26"
+},
+{
+  title: "BACKWALL | BWLL | 4 CHAROLAS | HEADER | HTU | CHARHTU |- ENE",
+  code: "OXXO_OXXO_BACKWALL_4CHAROLAS_HEADER_HTU_CHARHTU_ENE",
+  family: ["OXXO", "BWLL", "4 CHAROLAS", "HEADER", "HTU", "CHARHTU", "ENE"],
+  tags: "OXXO BWLL 4 CHAROLAS HEADER HTU CHARHTU ENE",
+  img: "ENE26_OXXO_4_CHAR_HEADER-HTU_CHARHTU.png",
+  categoria: "backwall",
+  cadena: "oxxo",
+  month: "ENE26"
+},
+{
+  title: "BACKWALL | BWLL | 4 CHAROLAS | HEADER | HTU | CHARHTU | ZYN |- ENE",
+  code: "OXXO_OXXO_BACKWALL_4CHAROLAS_HEADER_HTU_CHARHTU_ZYN_ENE",
+  family: ["OXXO", "BWLL", "4 CHAROLAS", "HEADER", "HTU", "CHARHTU", "ZYN", "ENE"],
+  tags: "OXXO BWLL 4 CHAROLAS HEADER HTU CHARHTU ZYN ENE",
+  img: "ENE26_OXXO_4_CHAR_HEADER-HTU_CHARHTU_ZYN.png",
+  categoria: "backwall",
+  cadena: "oxxo",
+  month: "ENE26"
+},
+{
+  title: "BACKWALL | BWLL | 4 CHAROLAS | HEADER | HTU | CHARHTU | ZYN | CHARZYN |- ENE",
+  code: "OXXO_OXXO_BACKWALL_4CHAROLAS_HEADER_HTU_CHARHTU_ZYN_CHARZYN_ENE",
+  family: ["OXXO", "BWLL", "4 CHAROLAS", "HEADER", "HTU", "CHARHTU", "ZYN", "CHARZYN", "ENE"],
+  tags: "OXXO BWLL 4 CHAROLAS HEADER HTU CHARHTU ZYN CHARZYN ENE",
+  img: "ENE26_OXXO_4_CHAR_HEADER-HTU_CHARHTU_ZYN_CHARZYN.png",
+  categoria: "backwall",
+  cadena: "oxxo",
+  month: "ENE26"
+},
+{
+  title: "BACKWALL | BWLL | 4 CHAROLAS | HEADER | HTU | CHARHTU | ZYN | CHARZYN25 |- ENE",
+  code: "OXXO_OXXO_BACKWALL_4CHAROLAS_HEADER_HTU_CHARHTU_ZYN_CHARZYN25_ENE",
+  family: ["OXXO", "BWLL", "4 CHAROLAS", "HEADER", "HTU", "CHARHTU", "ZYN", "CHARZYN25", "ENE"],
+  tags: "OXXO BWLL 4 CHAROLAS HEADER HTU CHARHTU ZYN CHARZYN25 ENE",
+  img: "ENE26_OXXO_4_CHAR_HEADER-HTU_CHARHTU_ZYN_CHARZYN25.png",
+  categoria: "backwall",
+  cadena: "oxxo",
+  month: "ENE26"
+},
+{
+  title: "BACKWALL | BWLL | 4 CHAROLAS | HEADER | HTU | ZYN |- ENE",
+  code: "OXXO_OXXO_BACKWALL_4CHAROLAS_HEADER_HTU_ZYN_ENE",
+  family: ["OXXO", "BWLL", "4 CHAROLAS", "HEADER", "HTU", "ZYN", "ENE"],
+  tags: "OXXO BWLL 4 CHAROLAS HEADER HTU ZYN ENE",
+  img: "ENE26_OXXO_4_CHAR_HEADER-HTU_ZYN.png",
+  categoria: "backwall",
+  cadena: "oxxo",
+  month: "ENE26"
+},
+{
+  title: "BACKWALL | BWLL | 4 CHAROLAS | HEADER | HTU | ZYN | CHARZYN |- ENE",
+  code: "OXXO_OXXO_BACKWALL_4CHAROLAS_HEADER_HTU_ZYN_CHARZYN_ENE",
+  family: ["OXXO", "BWLL", "4 CHAROLAS", "HEADER", "HTU", "ZYN", "CHARZYN", "ENE"],
+  tags: "OXXO BWLL 4 CHAROLAS HEADER HTU ZYN CHARZYN ENE",
+  img: "ENE26_OXXO_4_CHAR_HEADER-HTU_ZYN_CHARZYN.png",
+  categoria: "backwall",
+  cadena: "oxxo",
+  month: "ENE26"
+},
+{
+  title: "BACKWALL | BWLL | 4 CHAROLAS | HEADER | HTU | ZYN | CHARZYN25 |- ENE",
+  code: "OXXO_OXXO_BACKWALL_4CHAROLAS_HEADER_HTU_ZYN_CHARZYN25_ENE",
+  family: ["OXXO", "BWLL", "4 CHAROLAS", "HEADER", "HTU", "ZYN", "CHARZYN25", "ENE"],
+  tags: "OXXO BWLL 4 CHAROLAS HEADER HTU ZYN CHARZYN25 ENE",
+  img: "ENE26_OXXO_4_CHAR_HEADER-HTU_ZYN_CHARZYN25.png",
+  categoria: "backwall",
+  cadena: "oxxo",
+  month: "ENE26"
+},
+{
+  title: "BACKWALL | BWLL | 4 CHAROLAS | HEADER | ZYN |- ENE",
+  code: "OXXO_OXXO_BACKWALL_4CHAROLAS_HEADER_ZYN_ENE",
+  family: ["OXXO", "BWLL", "4 CHAROLAS", "HEADER", "ZYN", "ENE"],
+  tags: "OXXO BWLL 4 CHAROLAS HEADER ZYN ENE",
+  img: "ENE26_OXXO_4_CHAR_HEADER-ZYN.png",
+  categoria: "backwall",
+  cadena: "oxxo",
+  month: "ENE26"
+},
+{
+  title: "BACKWALL | BWLL | 4 CHAROLAS | HEADER | ZYN | CHARZYN |- ENE",
+  code: "OXXO_OXXO_BACKWALL_4CHAROLAS_HEADER_ZYN_CHARZYN_ENE",
+  family: ["OXXO", "BWLL", "4 CHAROLAS", "HEADER", "ZYN", "CHARZYN", "ENE"],
+  tags: "OXXO BWLL 4 CHAROLAS HEADER ZYN CHARZYN ENE",
+  img: "ENE26_OXXO_4_CHAR_HEADER-ZYN_CHARZYN.png",
+  categoria: "backwall",
+  cadena: "oxxo",
+  month: "ENE26"
+},
+{
+  title: "BACKWALL | BWLL | 4 CHAROLAS | HEADER | ZYN | CHARZYN25 |- ENE",
+  code: "OXXO_OXXO_BACKWALL_4CHAROLAS_HEADER_ZYN_CHARZYN25_ENE",
+  family: ["OXXO", "BWLL", "4 CHAROLAS", "HEADER", "ZYN", "CHARZYN25", "ENE"],
+  tags: "OXXO BWLL 4 CHAROLAS HEADER ZYN CHARZYN25 ENE",
+  img: "ENE26_OXXO_4_CHAR_HEADER-ZYN_CHARZYN25.png",
+  categoria: "backwall",
+  cadena: "oxxo",
+  month: "ENE26"
+},
+
+{
+  title: "BACKWALL | BWLL | 5 CHAROLAS | HEADER | BASE |- ENE",
+  code: "OXXO_OXXO_BACKWALL_5CHAROLAS_HEADER_BASE_ENE",
+  family: ["OXXO", "BWLL", "5 CHAROLAS", "HEADER", "BASE", "ENE"],
+  tags: "OXXO BWLL 5 CHAROLAS HEADER BASE ENE",
+  img: "ENE26_OXXO_5_CHAR_HEADER-.png",
+  categoria: "backwall",
+  cadena: "oxxo",
+  month: "ENE26"
+},
+{
+  title: "BACKWALL | BWLL | 5 CHAROLAS | HEADER | HTU |- ENE",
+  code: "OXXO_OXXO_BACKWALL_5CHAROLAS_HEADER_HTU_ENE",
+  family: ["OXXO", "BWLL", "5 CHAROLAS", "HEADER", "HTU", "ENE"],
+  tags: "OXXO BWLL 5 CHAROLAS HEADER HTU ENE",
+  img: "ENE26_OXXO_5_CHAR_HEADER-HTU.png",
+  categoria: "backwall",
+  cadena: "oxxo",
+  month: "ENE26"
+},
+{
+  title: "BACKWALL | BWLL | 5 CHAROLAS | HEADER | HTU | CHARHTU |- ENE",
+  code: "OXXO_OXXO_BACKWALL_5CHAROLAS_HEADER_HTU_CHARHTU_ENE",
+  family: ["OXXO", "BWLL", "5 CHAROLAS", "HEADER", "HTU", "CHARHTU", "ENE"],
+  tags: "OXXO BWLL 5 CHAROLAS HEADER HTU CHARHTU ENE",
+  img: "ENE26_OXXO_5_CHAR_HEADER-HTU_CHARHTU.png",
+  categoria: "backwall",
+  cadena: "oxxo",
+  month: "ENE26"
+},
+{
+  title: "BACKWALL | BWLL | 5 CHAROLAS | HEADER | HTU | CHARHTU | ZYN |- ENE",
+  code: "OXXO_OXXO_BACKWALL_5CHAROLAS_HEADER_HTU_CHARHTU_ZYN_ENE",
+  family: ["OXXO", "BWLL", "5 CHAROLAS", "HEADER", "HTU", "CHARHTU", "ZYN", "ENE"],
+  tags: "OXXO BWLL 5 CHAROLAS HEADER HTU CHARHTU ZYN ENE",
+  img: "ENE26_OXXO_5_CHAR_HEADER-HTU_CHARHTU_ZYN.png",
+  categoria: "backwall",
+  cadena: "oxxo",
+  month: "ENE26"
+},
+{
+  title: "BACKWALL | BWLL | 5 CHAROLAS | HEADER | HTU | CHARHTU | ZYN | CHARZYN |- ENE",
+  code: "OXXO_OXXO_BACKWALL_5CHAROLAS_HEADER_HTU_CHARHTU_ZYN_CHARZYN_ENE",
+  family: ["OXXO", "BWLL", "5 CHAROLAS", "HEADER", "HTU", "CHARHTU", "ZYN", "CHARZYN", "ENE"],
+  tags: "OXXO BWLL 5 CHAROLAS HEADER HTU CHARHTU ZYN CHARZYN ENE",
+  img: "ENE26_OXXO_5_CHAR_HEADER-HTU_CHARHTU_ZYN_CHARZYN.png",
+  categoria: "backwall",
+  cadena: "oxxo",
+  month: "ENE26"
+},
+{
+  title: "BACKWALL | BWLL | 5 CHAROLAS | HEADER | HTU | CHARHTU | ZYN | CHARZYN25 |- ENE",
+  code: "OXXO_OXXO_BACKWALL_5CHAROLAS_HEADER_HTU_CHARHTU_ZYN_CHARZYN25_ENE",
+  family: ["OXXO", "BWLL", "5 CHAROLAS", "HEADER", "HTU", "CHARHTU", "ZYN", "CHARZYN25", "ENE"],
+  tags: "OXXO BWLL 5 CHAROLAS HEADER HTU CHARHTU ZYN CHARZYN25 ENE",
+  img: "ENE26_OXXO_5_CHAR_HEADER-HTU_CHARHTU_ZYN_CHARZYN25.png",
+  categoria: "backwall",
+  cadena: "oxxo",
+  month: "ENE26"
+},
+{
+  title: "BACKWALL | BWLL | 5 CHAROLAS | HEADER | HTU | ZYN |- ENE",
+  code: "OXXO_OXXO_BACKWALL_5CHAROLAS_HEADER_HTU_ZYN_ENE",
+  family: ["OXXO", "BWLL", "5 CHAROLAS", "HEADER", "HTU", "ZYN", "ENE"],
+  tags: "OXXO BWLL 5 CHAROLAS HEADER HTU ZYN ENE",
+  img: "ENE26_OXXO_5_CHAR_HEADER-HTU_ZYN.png",
+  categoria: "backwall",
+  cadena: "oxxo",
+  month: "ENE26"
+},
+{
+  title: "BACKWALL | BWLL | 5 CHAROLAS | HEADER | HTU | ZYN | CHARZYN |- ENE",
+  code: "OXXO_OXXO_BACKWALL_5CHAROLAS_HEADER_HTU_ZYN_CHARZYN_ENE",
+  family: ["OXXO", "BWLL", "5 CHAROLAS", "HEADER", "HTU", "ZYN", "CHARZYN", "ENE"],
+  tags: "OXXO BWLL 5 CHAROLAS HEADER HTU ZYN CHARZYN ENE",
+  img: "ENE26_OXXO_5_CHAR_HEADER-HTU_ZYN_CHARZYN.png",
+  categoria: "backwall",
+  cadena: "oxxo",
+  month: "ENE26"
+},
+{
+  title: "BACKWALL | BWLL | 5 CHAROLAS | HEADER | HTU | ZYN | CHARZYN25 |- ENE",
+  code: "OXXO_OXXO_BACKWALL_5CHAROLAS_HEADER_HTU_ZYN_CHARZYN25_ENE",
+  family: ["OXXO", "BWLL", "5 CHAROLAS", "HEADER", "HTU", "ZYN", "CHARZYN25", "ENE"],
+  tags: "OXXO BWLL 5 CHAROLAS HEADER HTU ZYN CHARZYN25 ENE",
+  img: "ENE26_OXXO_5_CHAR_HEADER-HTU_ZYN_CHARZYN25.png",
+  categoria: "backwall",
+  cadena: "oxxo",
+  month: "ENE26"
+},
+{
+  title: "BACKWALL | BWLL | 5 CHAROLAS | HEADER | ZYN |- ENE",
+  code: "OXXO_OXXO_BACKWALL_5CHAROLAS_HEADER_ZYN_ENE",
+  family: ["OXXO", "BWLL", "5 CHAROLAS", "HEADER", "ZYN", "ENE"],
+  tags: "OXXO BWLL 5 CHAROLAS HEADER ZYN ENE",
+  img: "ENE26_OXXO_5_CHAR_HEADER-ZYN.png",
+  categoria: "backwall",
+  cadena: "oxxo",
+  month: "ENE26"
+},
+{
+  title: "BACKWALL | BWLL | 5 CHAROLAS | HEADER | ZYN | CHARZYN |- ENE",
+  code: "OXXO_OXXO_BACKWALL_5CHAROLAS_HEADER_ZYN_CHARZYN_ENE",
+  family: ["OXXO", "BWLL", "5 CHAROLAS", "HEADER", "ZYN", "CHARZYN", "ENE"],
+  tags: "OXXO BWLL 5 CHAROLAS HEADER ZYN CHARZYN ENE",
+  img: "ENE26_OXXO_5_CHAR_HEADER-ZYN_CHARZYN.png",
+  categoria: "backwall",
+  cadena: "oxxo",
+  month: "ENE26"
+},
+{
+  title: "BACKWALL | BWLL | 5 CHAROLAS | HEADER | ZYN | CHARZYN25 |- ENE",
+  code: "OXXO_OXXO_BACKWALL_5CHAROLAS_HEADER_ZYN_CHARZYN25_ENE",
+  family: ["OXXO", "BWLL", "5 CHAROLAS", "HEADER", "ZYN", "CHARZYN25", "ENE"],
+  tags: "OXXO BWLL 5 CHAROLAS HEADER ZYN CHARZYN25 ENE",
+  img: "ENE26_OXXO_5_CHAR_HEADER-ZYN_CHARZYN25.png",
+  categoria: "backwall",
+  cadena: "oxxo",
+  month: "ENE26"
+}
+
+
+
+
 ]
